92f984d4d240f078556a55fdfac4075b4d5b8808
SOFTINFO\barbub <bogdan.barbu@infoworld.ro>
2018-10-23 18:34:58 +0300

    add logo inputs and button

--- web-application/src/app/layout/auth/auth.module.ts
+++ web-application/src/app/layout/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth.routes';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    InputTextModule
   ],
   declarations: [LoginComponent]
 })
